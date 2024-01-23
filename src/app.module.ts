f1341dbf175c675951e86e2e827cc175d62cd731
cinta <cintatafur@gmail.com>
2024-01-23 12:42:42 +0100

    ✅ I correct and make validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+     ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule,
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URL),
+   
+   
   ],
   controllers: [],
   providers: [],
